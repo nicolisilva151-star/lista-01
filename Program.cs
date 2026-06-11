20e48900b38f07a8b879422be8765752d773a88a
nicoli <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-11 20:45:11 -0300

    Q1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("idade do visitante");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade< 16)
+{
+    Console.WriteLine("Entrada permitida somente com reponsavel");
+} 
+else {
+    Console.WriteLine("Entrada liberada");
+}
