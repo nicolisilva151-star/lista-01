33c09d71e5437612369e756cba23237f70603f5c
nicoli <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-15 19:31:13 -0300

    Q3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿Console.Write("Informe a nota final: ");
-int nota = int.Parse(Console.ReadLine()!);
+﻿Console.Write("Informe a temperatura atual: ");
+        double temperatura = Convert.ToDouble(Console.ReadLine());
 
-if (nota >= 7)
-{
-     Console.WriteLine("Estudante aprovado.");
-}
- else if (nota >= 5)
-{
-     Console.WriteLine("Estudante em recuperação.");
-}
-else
-{
-     Console.WriteLine("Estudante reprovado.");
-}
+        if (temperatura < 18)
+        {
+            Console.WriteLine("Ambiente frio.");
+        }
+        else if (temperatura <= 26)
+        {
+            Console.WriteLine("Ambiente confortável.");
+        }
+        else
+        {
+            Console.WriteLine("Ambiente quente.");
+        }
