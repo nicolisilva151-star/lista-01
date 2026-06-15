84bdab1b9bb08e0b55c4c8863b6626035699b744
nicoli <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-15 20:50:18 -0300

    Q14

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿     Console.Write("Informe o número da tabuada: ");
-        int numero = int.Parse(Console.ReadLine());
+﻿      Console.Write("Informe o primeiro valor: ");
+        double valor1 = double.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= 10; i++)
-        {
-            Console.WriteLine(numero + " x " + i + " = " + (numero * i));
-        }
+        Console.Write("Informe o segundo valor: ");
+        double valor2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o terceiro valor: ");
+        double valor3 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o quarto valor: ");
+        double valor4 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o quinto valor: ");
+        double valor5 = double.Parse(Console.ReadLine());
+
+        double soma = valor1 + valor2 + valor3 + valor4 + valor5;
+        double media = soma / 5;
+
+        Console.WriteLine("Soma dos valores: " + soma);
+        Console.WriteLine("Média dos valores: " + media);
     
