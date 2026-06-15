e863c0a99408250b8f9373dd39559bd592761fb4
nicoli <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-15 20:42:30 -0300

    Q11

--- Program.cs
+++ Program.cs
@@ -1,24 +1,7 @@
-﻿  Console.Write("Informe a idade do passageiro: ");
-        int idade = int.Parse(Console.ReadLine());
+﻿  Console.Write("Informe o número final da contagem: ");
+        int numeroFinal = int.Parse(Console.ReadLine());
 
-        Console.Write("Informe o valor normal da passagem: ");
-        double valorNormal = double.Parse(Console.ReadLine());
-
-        double valorPagar;
-
-        if (idade <= 5)
+        for (int i = 1; i <= numeroFinal; i++)
         {
-            valorPagar = 0;
+            Console.WriteLine(i);
         }
-        else if (idade >= 60)
-        {
-            valorPagar = valorNormal / 2;
-        }
-        else
-        {
-            valorPagar = valorNormal;
-        }
-
-        Console.WriteLine("Valor normal da passagem: R$ " + valorNormal.ToString("F2"));
-        Console.WriteLine("Valor a pagar: R$ " + valorPagar.ToString("F2"));
-    }
